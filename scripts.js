005463d5451bae5122a6ee9ec158b944b3c5c805
Mehul Singh <143124065+csemehul@users.noreply.github.com>
2024-07-01 07:56:21 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,16 +4,19 @@ const svg = d3.select("svg");
 const width = svg.attr("width");
 const height = svg.attr("height");
 
+// X scale
 const x = d3.scaleBand()
   .domain(d3.range(data.length))
   .range([0, width])
   .padding(0.1);
 
+// Y scale
 const y = d3.scaleLinear()
   .domain([0, d3.max(data)])
   .nice()
   .range([height, 0]);
 
+// Draw bars
 svg.append("g")
   .selectAll("rect")
   .data(data)
@@ -24,9 +27,11 @@ svg.append("g")
     .attr("height", d => y(0) - y(d))
     .attr("width", x.bandwidth());
 
+// Draw x-axis
 svg.append("g")
   .call(d3.axisBottom(x).tickFormat(i => i + 1).tickSizeOuter(0))
   .attr("transform", `translate(0,${height})`);
 
+// Draw y-axis
 svg.append("g")
   .call(d3.axisLeft(y));
